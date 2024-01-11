36bddcff1553d2f20faef39992c5563d27ddbbc1
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-01-11 23:14:27 +0100

    print-the-pattern-set-1

---
+++ src/print-the-pattern-set-1/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// https://www.geeksforgeeks.org/problems/print-the-pattern-set-1/1
+// 2023-08-31T05:48:35.000Z
+
+/**
+ * @param {{
+ *     readLine: () => string,
+ *     printList: (res: any[], n: number) => void
+ * }} input
+ */
+export default function main(input) {
+  let t = parseInt(input.readLine());
+  let i = 0;
+ 
+  for (; i < t; i++) {
+    let n = parseInt(input.readLine());
+    
+    let obj = new Solution();
+    obj.printPat(n);
+  }
+
+}
+
+class Solution {
+
+/**
+ * @param {number} n
+*/
+  printPat(n){
+    //code here
+  }
+}
